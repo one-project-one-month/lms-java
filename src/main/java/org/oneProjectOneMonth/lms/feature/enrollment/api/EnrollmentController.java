34c8e367ba1c0f4b0f7d5fac0b8d44441d5da160
thanthtooaung-coding <thanthtoo1285@gmail.com>
2025-02-15 08:59:31 +0700

    [Add] Add Enrollment Feature

--- src/main/java/org/oneProjectOneMonth/lms/feature/enrollment/api/EnrollmentController.java
+++ src/main/java/org/oneProjectOneMonth/lms/feature/enrollment/api/EnrollmentController.java
@@ -2,12 +2,37 @@ package org.oneProjectOneMonth.lms.feature.enrollment.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.oneProjectOneMonth.lms.feature.enrollment.dto.EnrollmentRequestDto;
+import org.oneProjectOneMonth.lms.feature.enrollment.dto.EnrollmentResponseDto;
+import org.oneProjectOneMonth.lms.feature.enrollment.domain.service.EnrollmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/${api.base.path}/${api.enrollment.base.path}")
 @RequiredArgsConstructor
 @Slf4j
 public class EnrollmentController {
+    private final EnrollmentService enrollmentService;
+
+    @PostMapping
+    public ResponseEntity<EnrollmentResponseD> enrollStudent(@RequestBody EnrollmentRequestDto request) {
+        log.info("Enrolling student {} to course {}", request.getStudentId(), request.getCourseId());
+        EnrollmentResponseDto response = enrollmentService.enrollStudent(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EnrollmentResponseDto> getEnrollment(@PathVariable Long id) {
+        log.info("Fetching enrollment with ID {}", id);
+        return ResponseEntity.ok(enrollmentService.getEnrollmentById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EnrollmentResponseDto>> getAllEnrollments() {
+        log.info("Fetching all enrollments");
+        return ResponseEntity.ok(enrollmentService.getAllEnrollments());
+    }
 }
