12ec48a603e15e433d45b63a2bd870715ee999e0
square <hlan1559@gmail.com>
2025-02-18 22:32:35 +0630

    Users update and delete

--- src/main/java/org/oneProjectOneMonth/lms/feature/instructor/domain/repository/InstructorRepository.java
+++ src/main/java/org/oneProjectOneMonth/lms/feature/instructor/domain/repository/InstructorRepository.java
@@ -1,7 +1,13 @@
 package org.oneProjectOneMonth.lms.feature.instructor.domain.repository;
 
+import java.util.Optional;
+
 import org.oneProjectOneMonth.lms.feature.instructor.domain.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+	@Query("SELECT i FROM Instructor i WHERE i.user.id = :userId")
+	Optional<Instructor> findInstructorByUserId(Long userId);
 }
+
